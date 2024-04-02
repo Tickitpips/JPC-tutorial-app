90ad0c2ede4989a3538b7f86658ec7ca7314ccc4
Tickitpips <keithjmuderwa@gmail.com>
2024-04-02 14:41:58 +0300

    Added more code to show the use of enum classes and generics

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -89,3 +89,50 @@ fun ComposeArticleAppPreview() {
         DisplayArticle()
     }
 }
+
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class TrueOrFalse(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
+//Refactored code to include generics
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+fun main(){
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}
+
+enum class Difficulty{
+    EASY, MEDIUM, HARD
+}
+
+class Question2<T>(
+    val questionText:String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+//fun main using enum
+fun main2(){
+    val question1 = Question2<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question2<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question2<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+}
